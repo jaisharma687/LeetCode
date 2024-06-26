0d2e33a4ee30e7d6ff61bd6a4334a5bcb84e18a9
Jai Sharma <jaisharma687@outlook.com>
2024-06-26 08:36:32 +0530

    50. Pow(x, n)

---
+++ 1 to 100/50_powx-n.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int mod = 1e9+7;
+
+/*
+    Explaination
+    https://www.youtube.com/watch?si=g_nvLVFWh7WXen3R&v=-mNn11iRZ5k&feature=youtu.be
+*/
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            x = 1/x;
+            n = abs(n);
+        }
+        double v = myPow(x,n/2);
+        if(n%2){
+            return x*v*v;
+        }else return v*v;
+    }
+};
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    Solution obj;
+}
