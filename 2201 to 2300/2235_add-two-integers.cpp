6b5e1a86918679f3b962be009a01eb57f62c145a
Jai Sharma <jaisharma687@outlook.com>
2024-07-01 09:12:41 +0530

    2235. Add Two Integers

---
+++ 2201 to 2300/2235_add-two-integers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int mod = 1e9+7;
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    Solution obj;
+}
