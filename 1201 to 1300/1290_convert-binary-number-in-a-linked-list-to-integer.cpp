d808eef2a1d0674cfb92d15062db46ed8738acb2
Jai Sharma <jaisharma687@outlook.com>
2024-07-06 00:33:29 +0530

    1290. Convert Binary Number in a Linked List to Integer

---
+++ 1201 to 1300/1290_convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int mod = 1e9+7;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res = 0;
+        ListNode*temp = head;
+        while(temp!=NULL){
+            res = res*2 + temp->val;
+            temp = temp->next;
+        }
+        return res;
+    }
+};
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    Solution obj;
+}
