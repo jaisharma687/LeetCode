12bc0b52de730489055668d191dbb5b36fc62118
Jai Sharma <jaisharma687@outlook.com>
2024-07-23 22:03:13 +0530

    242. Valid Anagram - Hash Map

--- 201 to 300/242_valid-anagram.cpp
+++ 201 to 300/242_valid-anagram.cpp
@@ -20,6 +20,22 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> sm,tm;
+        for(auto x:s) sm[x]++;
+        for(auto x:t) tm[x]++;
+        if(sm.size()!=tm.size()) return false;
+        for(auto &x:sm){
+            if(sm[x.first]!=tm[x.first]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
