1bbfcfc53a8752d2db2f1ac540f0bc77aa7a44f6
Jai Sharma <jaisharma687@outlook.com>
2024-07-29 23:20:07 +0530

    1395. Count Number of Teams - Optimized Solution

--- 1301 to 1400/1395_count-number-of-teams.cpp
+++ 1301 to 1400/1395_count-number-of-teams.cpp
@@ -21,6 +21,29 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int teams = 0;
+        int n = rating.size();
+        for(int i=0;i<n;i++){
+            int leftSmall = 0,leftBig = 0,rightSmall = 0,rightBig = 0;
+            int val = rating[i];
+            for(int j=i-1;j>=0;j--){
+                if(val > rating[j]) leftSmall++;
+                if(val < rating[j]) leftBig++;
+            }
+            for(int j=i+1;j<n;j++){
+                if(val > rating[j]) rightSmall++;
+                if(val < rating[j]) rightBig++;
+            }
+            teams += leftSmall*rightBig + leftBig*rightSmall;
+        }
+        return teams;
+    }
+};
+
 signed main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
