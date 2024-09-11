e72dd7e83f0d15b916216d056cc7635f41bac464
Jai Sharma <jaisharma687@outlook.com>
2024-09-11 23:15:12 +0530

    704. Binary Search - Java

---
+++ 701 to 800/Java/704 binary-search/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]<target) left=mid+1;
+            else if(nums[mid]>target) right=mid-1;
+            else return mid;
+        }
+        return -1;
+    }
+}
+
+public class Main{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
